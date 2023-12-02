a8f09ceae0c5a6cf13a0496410bad8c8326181d2
lutfinoor muhamad <lutfinoor.muhamad@gmail.com>
2023-12-02 10:39:00 +0700

    style: main content

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        
       </div>
       <h1>React + Vite Project by lutfinoorm</h1>
       <div className="card">
